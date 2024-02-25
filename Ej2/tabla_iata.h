83b33706976eec67cce7fc99889a746afd6057b6
Nicolas Graeff <ngraeff@fi.uba.ar>
2024-02-25 01:33:02 -0300

    add pre/post tabla file

--- Ej2/tabla_iata.h
+++ Ej2/tabla_iata.h
@@ -7,8 +7,9 @@ using namespace std;
 class Tabla_iata
 {
 private:
+
     //Atributos
-    struct Ciudad_iata{
+    struct Ciudad_iata{ //Utilizo un par (ciudad, codigo_iata) para poder vincularlo despues en el programa.
         string ciudad;
         string codigo_iata;
     };
@@ -17,19 +18,33 @@ private:
     int capacidad;
     
     //Metodos
+    //pre: -
+    //post: resimensiona la tabla al doble de su capacidad
     void redimensionar();
 
 public:
+    //Constructor
     Tabla_iata();
 
+    //Metodos
+
+    //pre: -
+    //post: agrega una ciudad con su respectivo codigo iata
     void agregar_ciudad_iata(string ciudad, string codigo_iata);
 
+    //pre: -
+    //post: devuelve true si la ciudad esta en la tabla, false en caso contrario
     bool pertenece(string ciudad);
 
+    //pre: -
+    //post: elimina la ciudad de la tabla
     void eliminiar(string ciudad);
 
+    //pre: -
+    //post: devuelve el codigo iata de la ciudad
     string buscar_iata (string ciudad);
 
+    //Destructor
     ~Tabla_iata();
 };
 #endif // TABLA_IATA_H
